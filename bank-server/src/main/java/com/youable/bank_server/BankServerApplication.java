a9e065e369d6afd0116c61d58a5671c1371c60e0
youable <youable.framios@gmail.com>
2024-11-22 00:27:37 +0900

    add: bank-server 초기 설정

---
+++ bank-server/src/main/java/com/youable/bank_server/BankServerApplication.java
@@ -0,0 +1,13 @@
+package com.youable.bank_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankServerApplication.class, args);
+	}
+
+}
---
+++ bank-server/src/test/java/com/youable/bank_server/BankServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youable.bank_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
