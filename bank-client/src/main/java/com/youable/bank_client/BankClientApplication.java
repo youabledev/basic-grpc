e38766e38adca9cf5763f3aae0e42632924d7d2b
youable <youable.framios@gmail.com>
2024-11-22 14:17:31 +0900

    add: bank-client

---
+++ bank-client/src/main/java/com/youable/bank_client/BankClientApplication.java
@@ -0,0 +1,13 @@
+package com.youable.bank_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankClientApplication.class, args);
+	}
+
+}
