e42e761786352b19c11a20468c91ad06281912f5
youable <youable.framios@gmail.com>
2024-11-22 14:47:11 +0900

    add: grpc 설정

---
+++ bank-client/src/test/java/com/youable/bank_client/BankClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youable.bank_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
